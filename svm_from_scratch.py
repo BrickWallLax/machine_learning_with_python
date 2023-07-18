701b5242713c6d5cd639dd3add9243c8e0423a06
BrickWallLax <69017601+BrickWallLax@users.noreply.github.com>
2023-07-18 16:20:38 -0400

    Up to episode 28 of the series - Finished

--- svm_from_scratch.py
+++ svm_from_scratch.py
@@ -4,11 +4,11 @@ import numpy as np
 
 style.use('ggplot')
 
-
 class Support_Vector_Machine:
     def __init__(self, visualization=True):
         self.visualization = visualization
         self.colors = {1: 'r', -1: 'b'}
+        #  Don't worry about this
         if self.visualization:
             self.fig = plt.figure()
             self.ax = self.fig.add_subplot(1, 1, 1)
@@ -19,12 +19,12 @@ class Support_Vector_Machine:
         # { ||w||: [w, b]}
         opt_dict = {}
 
-        '''
-        transforms = [[1,1],
-                      [-1,1],
-                      [-1,-1],
-                      [1,-1]]
-        '''
+        # # Transform the data after the equation b/c data comes out as [+, +] and we want to test all vectors instead of
+        # # just positive ones
+        # transform = [[1, 1],
+        #              [-1, -1],
+        #              [-1, 1],
+        #              [1, -1]]
 
         rot_matrix = lambda theta: np.array([[np.cos(theta), -np.sin(theta)],
                                             [np.sin(theta), np.cos(theta)]])
@@ -57,35 +57,33 @@ class Support_Vector_Machine:
         # We don't need to take as small of steps
         # with b as we do w
         b_multiple = 5
-        latest_optimum = self.max_feature_value * 10
+        latest_optimum = self.max_feature_value*10
 
         for step in step_sizes:
             # Start the first swing at max value
             w = np.array([latest_optimum, latest_optimum])
-            # we can do this because convex
-            optimized = False
-            while not optimized:
-                for b in np.arange(-1 * (self.max_feature_value * b_range_multiple),
-                                   self.max_feature_value * b_range_multiple,
-                                   step * b_multiple):
+            optimised = False
+            # Check
+            while not optimised:
+                for b in np.arange(-1*(self.max_feature_value*b_range_multiple),
+                                   self.max_feature_value*b_range_multiple,
+                                   step*b_multiple):
                     for transformation in transforms:
-                        w_t = w * transformation
+                        w_t = w*transformation
                         found_option = True
                         # Weakest link in the SVM fundamentally
                         # SMO attempts to fix this a bit
                         # yi(xi.w + b) >= 1
-                        #
-                        # #### add a break here later...
                         for i in self.data:
                             for xi in self.data[i]:
                                 yi = i
-                                if not yi * (np.dot(w_t, xi) + b) >= 1:
+                                # If yi*(np.dot(w_t, xi) + b) < 1, don't add it to the opt_dict
+                                if not yi*(np.dot(w_t, xi) + b) >= 1:
                                     found_option = False
                                 # print(xi, ':', yi*(np.dot(w_t, xi) + b))
                         if found_option:
                             # np.linalg.norm(w_t) is the magnitude of the vector
                             opt_dict[np.linalg.norm(w_t)] = [w_t, b]
-
                 if w[0] < 0:
                     optimised = True
                     print('Optimized a step')
@@ -94,40 +92,30 @@ class Support_Vector_Machine:
                     w = w - step
             # Sorted list of magnitudes, sort them from lowest to highest
             norms = sorted([n for n in opt_dict])
-            # print(norms)
-            # print(opt_dict)
-            # ||w|| : [w,b]
             opt_choice = opt_dict[norms[0]]
             # ||w|| : [w, b]
             self.w = opt_choice[0]
             self.b = opt_choice[1]
-            latest_optimum = opt_choice[0][0] + step * 2
-            print(self.w / np.linalg.norm(self.w))
-        '''    
-        for i in self.data:
-            for xi in self.data[i]:
-                yi=i
-                print(xi,':',yi*(np.dot(self.w,xi)+self.b))
-        '''
+            # Reset the latest optimum to smaller value
+            latest_optimum = opt_choice[0][0]+step*2
 
     # Checks whether the class is a positive or negative
     def predict(self, features):
-        # sign( x.w+b )
+        # sign(x.w + b)
         classification = np.sign(np.dot(np.array(features), self.w) + self.b)
         if classification != 0 and self.visualization:
             self.ax.scatter(features[0], features[1], s=200, marker='*', c=self.colors[classification])
         return classification
 
-    def visualize(self):
-        [[self.ax.scatter(x[0], x[1], s=100, color=self.colors[i]) for x in data_dict[i]] for i in data_dict]
+    def visualise(self):
+        [[self.ax.scatter(x[0], x[1], s=100, c=self.colors[i]) for x in data_dict[i]] for i in data_dict]
 
-        # hyperplane = x.w+b
-        # v = x.w+b
+        # Hyperplane = x.w + b
+        # v = x.w + b
         # psv = 1
-        # nsv = -1
-        # dec = 0
+
         def hyperplane(x, w, b, v):
-            return (-w[0] * x - b + v) / w[1]
+            return (-w[0]*x + b + v) / w[1]
 
         data_range = (self.min_feature_value * 0.9, self.max_feature_value * 1.1)
         hyp_x_min = data_range[0]
@@ -154,37 +142,22 @@ class Support_Vector_Machine:
         plt.show()
 
 
-'''   
-data_dict = {-1:np.array([[1,7],
-                          [2,8],
-                          [3,8],]),
-
-             1:np.array([[5,1],
-                         [6,-1],
-                         [7,3],])}
-data_dict = {-1:np.array([[1,1],
-                          [2,1],
-                          [3,1],]),
-
-             1:np.array([[1,2],
-                         [2,2],
-                         [3,2],])}
-'''
-
-data_dict = {1: [(1, 1), (2, 1.5), (3, 2)], -1: [(3, 0), (4, 0.5), (5, 1)]}
+data_dict = {-1: np.array([[1, 1],
+                           [2, 1],
+                           [3, 1]]),
+              1: np.array([[1, 4],
+                           [2, 4],
+                           [3, 4]])}
 
 svm = Support_Vector_Machine()
 svm.fit(data=data_dict)
 
 predict_us = [[0, 10],
               [1, 3],
-              [3, 4],
-              [3, 5],
               [5, 5],
               [5, 6],
               [6, -5],
               [5, 8]]
-
 for p in predict_us:
     svm.predict(p)
 svm.visualise()
