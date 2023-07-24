2204752f7454ef2e2bb292a39bc5d247f0b50402
BrickWallLax <69017601+BrickWallLax@users.noreply.github.com>
2023-07-23 22:46:52 -0400

    Episode 36 of the series - Complete

--- k_mean_titanic.py
+++ k_mean_titanic.py
@@ -3,36 +3,56 @@ from matplotlib import style
 import numpy as np
 from sklearn.cluster import KMeans
 from sklearn import preprocessing
-from sklearn.model_selection import train_test_split
 import pandas as pd
 style.use('ggplot')
 
 df = pd.read_excel('titanic_data.xlsx', engine='openpyxl')
-df.drop(['body', 'name'], axis=1, inplace=True)
+df.apply(pd.to_numeric, errors='ignore')
+df.drop(['body', 'name', 'boat', 'ticket'], axis=1, inplace=True)
 df.fillna(0, inplace=True)
 
 
 def handle_nonnumerical_data(df):
     columns = df.columns.values
+    text_digit_vals = {}  # Initialize the dictionary outside the loop
 
     for column in columns:
-        text_digit_vals = {}
-
-        def convert_to_int(val):
-            return text_digit_vals[val]
-
         if df[column].dtype != np.int64 and df[column].dtype != np.float64:
             column_contents = df[column].values.tolist()
             unique_elements = set(column_contents)
+
             x = 0
             for unique in unique_elements:
                 if unique not in text_digit_vals:
+                    # Create a new key for each new value
                     text_digit_vals[unique] = x
                     x += 1
 
-            df[column] = list(map(convert_to_int, df[columns]))
+            df[column] = list(map(lambda val: text_digit_vals[val], df[column]))
+
     return df
 
 
 df = handle_nonnumerical_data(df)
-print(df.head())
+
+X = np.array(df.drop(['survived'], axis=1).astype(float))
+X = preprocessing.scale(X)
+y = np.array(df['survived'])
+
+clf = KMeans(n_clusters=2)
+clf.fit(X)
+
+df.drop(['sex'], axis=1, inplace=True)
+
+correct = 0
+for i in range(len(X)):
+    predict_me = np.array(X[i].astype(float))
+    predict_me.reshape(1, len(predict_me))
+    prediction = clf.predict([predict_me])
+    if prediction[0] == y[i]:
+        correct += 1
+
+print(correct/len(X))
+
+
+
